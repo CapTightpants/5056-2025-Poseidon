2408c10396f835f73a50ca5636c6cbc8a7b01bea
CapTightpants <88792220+CapTightpants@users.noreply.github.com>
2025-03-08 07:34:07 -0500

    lift logic

--- src/main/java/frc/robot/Commands/CommandMultiPosition.java
+++ src/main/java/frc/robot/Commands/CommandMultiPosition.java
@@ -12,6 +12,7 @@ public class CommandMultiPosition extends Command {
     private final CoralArm m_coral;
     private final AlgaeArm m_algaeArm;
     private final kLiftPosition m_position;
+    private boolean isFinished = false;
 
     public CommandMultiPosition(Lift lift, CoralArm coralArm, AlgaeArm algaeArm, kLiftPosition position) {
         m_lift = lift;
@@ -24,33 +25,38 @@ public class CommandMultiPosition extends Command {
 
     @Override
     public void execute() {
-        // if ((m_position == kLiftPosition.Base) && (m_lift.getLiftPosition(m_position) != true)) {
-        //     Commands.sequence(
-        //         new CommandPositionLift(m_lift, m_position),
-        //         new CommandPositionCoral(m_coral, m_position)
-        //     );
-        //     new CommandPositionAlgae(m_algaeArm, m_position);
-        // }
+        if ((m_position == kLiftPosition.Base) && (m_lift.getLiftPosition(m_position) != true)) {
+            Commands.sequence(
+                new CommandPositionLift(m_lift, m_position),
+                new CommandPositionCoral(m_coral, m_position),
+                new CommandPositionAlgae(m_algaeArm, m_position)
+            );
+        }
 
-        // if ((m_CoralArm.getCoralPosition(kLiftPosition.Base)) && ((m_position != kLiftPosition.processor) || (m_position != kLiftPosition.Start))) {
-        //     Commands.sequence(
-        //         new CommandPositionCoral(m_coral, m_position),
-        //         new CommandPositionLift(m_lift, m_position)
-        //     );
-        //     new CommandPositionAlgae(m_algaeArm, m_position);
-        // }
+        if (((m_coral.getCoralPosition(kLiftPosition.Base)) || (m_algaeArm.getAlgaePosition(kLiftPosition.Base))) && ((m_position != kLiftPosition.processor) || (m_position != kLiftPosition.Start))) {
+            Commands.sequence(
+                new CommandPositionCoral(m_coral, m_position),
+                new CommandPositionAlgae(m_algaeArm, m_position),
+                new CommandPositionLift(m_lift, m_position)
+            );
+        }
 
-        // else {
-            // new CommandPositionAlgae(m_algaeArm, m_position);
-            // new CommandPositionCoral(m_coral, m_position);
-            // new CommandPositionLift(m_lift, m_position);
-        // }
-        m_algaeArm.AlgaeArmPosition(m_position);
-        m_coral.CoralArmPosition(m_position);
-        m_lift.setLiftPosition(m_position);
+        else {
+            new CommandPositionAlgae(m_algaeArm, m_position);
+            new CommandPositionCoral(m_coral, m_position);
+            new CommandPositionLift(m_lift, m_position);
+        }
+        // m_algaeArm.AlgaeArmPosition(m_position);
+        // m_coral.CoralArmPosition(m_position);
+        // m_lift.setLiftPosition(m_position);
+        isFinished = true;
     }
 
     @Override public boolean isFinished() {
-        return true;
+        return isFinished;
+    }
+
+    @Override public void end(boolean interrupted) {
+        isFinished = false;
     }
 }
